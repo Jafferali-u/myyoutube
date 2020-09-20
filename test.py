a76ea47065d2f61ecded1a7f1c3e56050d535b66
Jafferali-u <jafferali.u1@gmail.com>
2020-09-20 15:49:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import time
 import subprocess
 import os
 
-subprocess.Popen(["firefox", "https://youtu.be/tuIZok81iLk"])
-time.sleep(30)
+subprocess.Popen(["firefox", "https://youtu.be/sQSbsBu2gCU"])
+time.sleep(100)
+subprocess.Popen(["firefox", "https://youtu.be/IkjteJNcKqs"])
+time.sleep(400)
 os.system('pkill firefox')
