2d6a1ca2cd6a08f3fcbcaf1295df715eb328c411
Jafferali-u <jafferali.u1@gmail.com>
2020-09-23 10:52:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ for i in range(5):
   subprocess.Popen(["firefox", "https://youtu.be/j9x8JCp2YEo"])
   time.sleep(450)
   os.system('pkill firefox')
+  subprocess.Popen(["firefox", "https://youtu.be/vg795v1N2nw"])
+  time.sleep(200)
   time.sleep(30)
 
-  
